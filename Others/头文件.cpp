3f1c69d991c3c58430475193e7fcdd5344847ccf
FredySu <56098046+FredySu@users.noreply.github.com>
2019-10-08 22:15:15 +0800

    Add files via upload

---
+++ Others/头文件.cpp
@@ -0,0 +1,20 @@
+// ACM/ICPC竞赛头文件（C/C++）
+#include<bits/stdc++.h>包含C++所有头文件。 
+
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <stack>
+#include <vector>
+#include <list>
+#include <set>
+#include <map>
+#include <ctime>
+#include <deque>
+#include <bitset>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iomanip>
